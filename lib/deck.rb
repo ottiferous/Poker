65df4ca504409c5380fb2d05836b7ce34abbe09b
Andrew Marrone <github@andrewmarrone.com>
2014-02-07 14:14:46 -0800

    draw method takes optional argument to get multiple cards for a hand

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,14 +1,26 @@
 class Deck
-  
-  attr_reader :count
+
   def initialize
     @cards = []
-    %w[:club :diamond :heart :spade].each do |suit|
+    [:club, :diamond, :heart, :spade].each do |suit|
       %w[A 2 3 4 5 6 7 8 9 10 J Q K].each do |rank|
-        @cards << Card.new(suit, rank)
+        @cards << Card.new(rank, suit)
       end
     end
-    @count = @cards.count
+    @cards.shuffle!
+  end
+
+  def count
+    @cards.count
+  end
+  
+  def draw(num=1)
+    @cards.pop(num)
+  end
+  
+  def shuffle!
+    @cards.shuffle
+    true
   end
   
 end
--- spec/deck_rspec.rb
+++ spec/deck_rspec.rb
@@ -10,8 +10,21 @@ describe Deck do
   it 'has 52 cards by default' do
     expect(deck.count).to eql(52)
   end
-  it 'has no duplicates by default'
-  it 'changes order when shuffled'
+  it 'has no duplicates in a 52-card deck' do
+    unique_array = []
+    52.times { unique_array << deck.draw }
+    unique_array = unique_array.uniq
+    expect(unique_array.size).to eql(52)
+  end
+  it 'loses a card each time one is drawn' do
+    hand_array = []
+    rand(20).times { hand_array << deck.draw }
+    expect(hand_array.count + deck.count).to eql(52)
+  end
+  it 'changes order when shuffled' do
+    expect(deck).not_to eql(deck.shuffle!)
+  end
+  
   it 'may have any positive number of cards'
 
   
